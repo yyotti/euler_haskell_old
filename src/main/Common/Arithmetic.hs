f333d4f0b8e90c979a3a0d7d4b957bd243a17f4f
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-09 20:11:51 +0900

    [refac] 関数を共通に移動
    - primes
    - isPrime
    - primeFactors

--- src/main/Common/Arithmetic.hs
+++ src/main/Common/Arithmetic.hs
@@ -1,3 +1,20 @@
 module Common.Arithmetic where
-  fib :: [Integer]
-  fib = 1 : 1 : (map (\ (a, b) -> a + b) $ zip fib $ tail fib)
+import Data.List
+
+fib :: [Integer]
+fib = 1 : 1 : (map (\ (a, b) -> a + b) $ zip fib $ tail fib)
+
+primes :: [Integer]
+primes = 2 : filter isPrime [3, 5 ..]
+
+isPrime :: Integer -> Bool
+isPrime n | n < 2 = False
+          | otherwise = all ((/= 0) . mod n) $ ps
+  where ps = takeWhile ((<= n) . (^(2 :: Integer))) primes
+
+primeFactors :: Integer -> [Integer]
+primeFactors n | n < 2 = []
+               | otherwise = case find ((== 0) . mod n) ps of
+                                  Just p -> p : (primeFactors $ n `div` p)
+                                  Nothing -> [n]
+  where ps = takeWhile ((<= n) . (^(2 :: Integer))) primes
--- src/main/Problem/P003.hs
+++ src/main/Problem/P003.hs
@@ -1,6 +1,6 @@
 module Problem.P003 where
 import Data.Time
-import Data.List
+import Common.Arithmetic
 
 {-
 - Largest prime factor
@@ -15,21 +15,6 @@ import Data.List
 - 6857
 - time:0.007092s
 -}
-
-primes :: [Integer]
-primes = 2 : filter isPrime [3, 5 ..]
-
-isPrime :: Integer -> Bool
-isPrime n | n < 2 = False
-          | otherwise = all ((/= 0) . mod n) $ ps
-  where ps = takeWhile ((<= n) . (^(2 :: Integer))) primes
-
-primeFactors :: Integer -> [Integer]
-primeFactors n | n < 2 = []
-               | otherwise = case find ((== 0) . mod n) ps of
-                                  Just p -> p : (primeFactors $ n `div` p)
-                                  Nothing -> [n]
-  where ps = takeWhile ((<= n) . (^(2 :: Integer))) primes
 
 solve :: Integer -> Integer
 solve = last . primeFactors

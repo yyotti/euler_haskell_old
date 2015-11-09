aba957d8356e9ee700cd18f3100473e3a24ee179
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-09 17:53:34 +0900

    [P003] 回答

--- src/main/Problem/P003.hs
+++ src/main/Problem/P003.hs
@@ -1,5 +1,6 @@
 module Problem.P003 where
 import Data.Time
+import Data.List
 
 {-
 - Largest prime factor
@@ -7,13 +8,31 @@ import Data.Time
 -}
 
 {-
-- [回答方針]
+- [方針1]
+- 素因数分解を行い、出てきた素因数の最大値を答えとする。
 -
 - [結果]
--
+- 6857
+- time:0.007092s
 -}
+
+primes :: [Integer]
+primes = 2 : filter isPrime [3, 5 ..]
+
+isPrime :: Integer -> Bool
+isPrime n | n < 2 = False
+          | otherwise = all ((/= 0) . mod n) $ ps
+  where ps = takeWhile ((<= n) . (^(2 :: Integer))) primes
+
+primeFactors :: Integer -> [Integer]
+primeFactors n | n < 2 = []
+               | otherwise = case find ((== 0) . mod n) ps of
+                                  Just p -> p : (primeFactors $ n `div` p)
+                                  Nothing -> [n]
+  where ps = takeWhile ((<= n) . (^(2 :: Integer))) primes
+
 solve :: Integer -> Integer
-solve _ = undefined
+solve = last . primeFactors
 
 main :: IO ()
 main = do
--- src/test/Problem/P003Spec.hs
+++ src/test/Problem/P003Spec.hs
@@ -8,6 +8,58 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "primes" $ do
+    it "first 10 terms are [2,3,5,7,11,13,19,23,29,31]" $ do
+      take 10 primes `shouldBe` [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+  describe "isPrime" $ do
+    it "returns False when n = 1" $ do
+      isPrime 1 `shouldBe` False
+    it "returns True when n = 2" $ do
+      isPrime 2 `shouldBe` True
+    it "returns True when n = 3" $ do
+      isPrime 3 `shouldBe` True
+    it "returns False when n = 4" $ do
+      isPrime 4 `shouldBe` False
+    it "returns True when n = 5" $ do
+      isPrime 5 `shouldBe` True
+    it "returns False when n = 6" $ do
+      isPrime 6 `shouldBe` False
+    it "returns True when n = 7" $ do
+      isPrime 7 `shouldBe` True
+    it "returns False when n = 8" $ do
+      isPrime 8 `shouldBe` False
+    it "returns False when n = 9" $ do
+      isPrime 9 `shouldBe` False
+    it "returns False when n = 10" $ do
+      isPrime 10 `shouldBe` False
+    it "returns True when n = 11" $ do
+      isPrime 11 `shouldBe` True
+
+  describe "primeFactors" $ do
+    it "returns [] when n = 1" $ do
+      primeFactors 1 `shouldBe` []
+    it "returns [2] when n = 2" $ do
+      primeFactors 2 `shouldBe` [2]
+    it "returns [3] when n = 3" $ do
+      primeFactors 3 `shouldBe` [3]
+    it "returns [2,2] when n = 4" $ do
+      primeFactors 4 `shouldBe` [2, 2]
+    it "returns [5] when n = 5" $ do
+      primeFactors 5 `shouldBe` [5]
+    it "returns [2,3] when n = 6" $ do
+      primeFactors 6 `shouldBe` [2, 3]
+    it "returns [7] when n = 7" $ do
+      primeFactors 7 `shouldBe` [7]
+    it "returns [2,2,2] when n = 8" $ do
+      primeFactors 8 `shouldBe` [2, 2, 2]
+    it "returns [3,3] when n = 9" $ do
+      primeFactors 9 `shouldBe` [3, 3]
+    it "returns [2,5] when n = 10" $ do
+      primeFactors 10 `shouldBe` [2, 5]
+    it "returns [11] when n = 11" $ do
+      primeFactors 11 `shouldBe` [11]
+
   describe "solve" $ do
     it "returns 29 when n = 13195" $ do
       solve 13195 `shouldBe` 29

45db11e983d2fa9e528f2e4a5e3d5a18e653506a
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-09 12:46:33 +0900

    Problem 2 に回答
    正面からぶつかったら、案の定遅すぎる。

--- src/main/Problem/P002.hs
+++ src/main/Problem/P002.hs
@@ -6,8 +6,24 @@ import Data.Time
 - https://projecteuler.net/problem=2
 -}
 
+{-
+- [方針1]
+- フィボナッチ数列の定義に従って第n項を再帰的に求める関数を作り、400万以下の
+- 偶数値の項の和をとる。
+- 便宜上、n < 1の場合は0とする。(エラー処理がめんどくさいため)
+-
+- [結果]
+- 4613732
+- time:13.474108s
+-}
+fib :: Int -> Integer
+fib 1 = 1
+fib 2 = 2
+fib n | n >= 3 = fib (n - 1) + fib (n - 2)
+      | otherwise = 0
+
 solve :: Int -> Integer
-solve _ = undefined
+solve n = sum $ filter even $ takeWhile ((<= n) . fromIntegral) $ map fib [1..]
 
 main :: IO ()
 main = do
--- src/test/Problem/P002Spec.hs
+++ src/test/Problem/P002Spec.hs
@@ -8,6 +8,22 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "fib" $ do
+    it "returns 0 when n = 0" $ do
+      fib 0 `shouldBe` 0
+    it "returns 1 when n = 1" $ do
+      fib 1 `shouldBe` 1
+    it "returns 2 when n = 2" $ do
+      fib 2 `shouldBe` 2
+    it "returns 3 when n = 3" $ do
+      fib 3 `shouldBe` 3
+    it "returns 5 when n = 4" $ do
+      fib 4 `shouldBe` 5
+    it "returns 8 when n = 5" $ do
+      fib 5 `shouldBe` 8
+    it "returns 13 when n = 6" $ do
+      fib 6 `shouldBe` 13
+
   describe "solve" $ do
     it "returns 44 when n = 100" $ do
       solve 100 `shouldBe` 44

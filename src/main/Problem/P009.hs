21a086dd4d5165e85c22cd4f9d4a791aaab7c2b5
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-11 09:01:19 +0900

    [P009] 回答

--- src/main/Problem/P009.hs
+++ src/main/Problem/P009.hs
@@ -8,16 +8,46 @@ import Data.Time
 
 {-
 - [方針1]
+- cが斜辺、a,bが直角を挟む2つの辺なので、大小関係として
+-   a <= b < c
+- としてよい。
+- また
+-   a + b + c = n
+- とすると、
+-   c = n - a - b
+- であるから、
+-   b < n - a - b
+- より
+-   b < (n - a)/2
+- であり、
+-   a <= b < (n - a)/2
+- より
+-   a <= n/3
+- である。
+-
+- あとは上記の関係を満たす(a, b, c)について、ピタゴラス数に
+- なるものを探し積を求めればよい。
 -
 - [結果]
+- 31875000
+- time:0.113426s
 -}
+
+isPythagoreanTripret :: (Int, Int, Int) -> Bool
+isPythagoreanTripret (a, b, c) = a * a + b * b == c * c
+
+findSpecialPythagoreanTriprets :: Int -> [(Int, Int, Int)]
+findSpecialPythagoreanTriprets n  = filter isPythagoreanTripret $ concatMap (\ a -> map (\ b -> (a, b, n - a - b)) [a .. ((n - a) `div` 2)]) [1..(n `div` 3)]
+
 solve :: Int -> Int
-solve _ = undefined
+solve n = case findSpecialPythagoreanTriprets n of
+             [] -> 0
+             ((a, b, c):_) -> a * b * c
 
 main :: IO ()
 main = do
   x <- getCurrentTime
-  print $ solve
+  print $ solve 1000
   y <- getCurrentTime
 
   putStr "time:"
--- src/test/Problem/P009Spec.hs
+++ src/test/Problem/P009Spec.hs
@@ -8,6 +8,32 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "isPythagoreanTripret" $ do
+    it "returns False when (a,b,c) = (1,1,1)" $ do
+      isPythagoreanTripret (1, 1, 1) `shouldBe` False
+    it "returns False when (a,b,c) = (1,1,2)" $ do
+      isPythagoreanTripret (1, 1, 2) `shouldBe` False
+    it "returns False when (a,b,c) = (1,2,3)" $ do
+      isPythagoreanTripret (1, 2, 3) `shouldBe` False
+    it "returns True when (a,b,c) = (3,4,5)" $ do
+      isPythagoreanTripret (3, 4, 5) `shouldBe` True
+    it "returns False when (a,b,c) = (4,5,6)" $ do
+      isPythagoreanTripret (4, 5, 6) `shouldBe` False
+    it "returns True when (a,b,c) = (6,8,10)" $ do
+      isPythagoreanTripret (6, 8, 10) `shouldBe` True
+    it "returns True when (a,b,c) = (5,12,13)" $ do
+      isPythagoreanTripret (5, 12, 13) `shouldBe` True
+
+  describe "findSpecialPythagoreanTriprets" $ do
+    it "returns [] when n = 3" $ do
+      findSpecialPythagoreanTriprets 3 `shouldBe` []
+    it "returns [(3,4,5)] when n = 12" $ do
+      findSpecialPythagoreanTriprets 12 `shouldBe` [(3, 4, 5)]
+    it "returns [(6,8,10)] when n = 24" $ do
+      findSpecialPythagoreanTriprets 24 `shouldBe` [(6, 8, 10)]
+    it "returns [(5,12,13)] when n = 30" $ do
+      findSpecialPythagoreanTriprets 30 `shouldBe` [(5, 12, 13)]
+
   describe "solve" $ do
     it "returns 31875000 when n = 1000" $ do
       solve 1000 `shouldBe` 31875000

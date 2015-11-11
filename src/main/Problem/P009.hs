f8a9eba7e9473b5d0f2534ea9ffe1a64e105bed3
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-11 10:02:14 +0900

    [P009] 高速化
    原始ピタゴラス数を用いることで大幅な高速化に成功

--- src/main/Problem/P009.hs
+++ src/main/Problem/P009.hs
@@ -31,18 +31,57 @@ import Data.Time
 - [結果]
 - 31875000
 - time:0.113426s
+-
+- [コミット]
+- 21a086d
 -}
 
-isPythagoreanTripret :: (Int, Int, Int) -> Bool
-isPythagoreanTripret (a, b, c) = a * a + b * b == c * c
+{-
+- [方針2]
+- (x, y, z)を原始ピタゴラス数とする。
+- wを自然数とすれば、(a, b, c)は
+-   (a, b, c) = (wx, wy, wz)
+- と表せる。
+- よって原始ピタゴラス数を列挙し、
+-   a + b + c = w(x + y + z) = k  (kは与えられた a + b + c の値)
+- となる(w, x, y, z)を抽出すればよい。
+-
+- 原始ピタゴラス数を列挙する方法は
+-   (x, y, z) = (m^2 - n^2, 2mn, m^2 + n^2)
+-   ただし m,n ∈  N, m > n, (m - n) ≡ 1 (mod 2), gcd(m, n) = 1
+- であるから、
+-   a + b + c = w(m^2 - n^2 + 2mn + m^2 + n^2)
+-             = w(2m^2 + 2mn)
+-             = 2wm(m + n) = k
+- よって、
+-   k ≡  0 (mod 2m(m + n))
+- となる(m,n)を求め、w = k/(2m(m + n))として(a, b, c)を求めればよい。
+-
+- なお、kが奇数の場合には条件を満たす(m,n)は存在しない。
+- また、
+-   2wm(m + n) = k
+- より、w = 1、n = 0として
+-   2m^2 <= k
+- がmの上限である。
+-
+- [結果]
+- 31875000
+- time:0.000201s
+-}
 
 findSpecialPythagoreanTriprets :: Int -> [(Int, Int, Int)]
-findSpecialPythagoreanTriprets n  = filter isPythagoreanTripret $ concatMap (\ a -> map (\ b -> (a, b, n - a - b)) [a .. ((n - a) `div` 2)]) [1..(n `div` 3)]
+findSpecialPythagoreanTriprets k | odd k = []
+                                 | otherwise = map toABC $ filter (\ (m, n) -> k `mod` (2 * m * (m + n)) == 0) mns
+  where ms = takeWhile (\ m -> 2 * m * m <= k) [1..]
+        ns m = filter (\ n -> odd (m - n) && gcd m n == 1) [1..(m - 1)]
+        mns = concatMap (\ m -> map (\ n -> (m, n)) $ ns m) ms
+        toABC (m, n) = let w = k `div` (2 * m * (m + n))
+                           in (w * (m * m - n * n), w * 2 * m * n, w * (m * m + n * n))
 
 solve :: Int -> Int
 solve n = case findSpecialPythagoreanTriprets n of
-             [] -> 0
-             ((a, b, c):_) -> a * b * c
+               [] -> 0
+               ((a, b, c):_) -> a * b * c
 
 main :: IO ()
 main = do

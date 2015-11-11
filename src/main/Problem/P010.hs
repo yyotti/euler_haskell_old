0d02e694f27b032bcc2b5d01f0949534028ce54f
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-11 12:26:40 +0900

    [P010] 高速化
    エラトステネスの篩にしてみたら速度が約2倍になった

--- src/main/Problem/P010.hs
+++ src/main/Problem/P010.hs
@@ -1,6 +1,5 @@
 module Problem.P010 where
 import Data.Time
-import Common.Arithmetic
 
 {-
 - Summation of primes
@@ -14,9 +13,29 @@ import Common.Arithmetic
 - [結果]
 - 142913828922
 - time:36.233071s
+-
+- [コミット]
+- ce3ae63
 -}
-solve :: Integer -> Integer
-solve n = sum $ takeWhile (<= n) primes
+
+{-
+- [方針2]
+- 最大値が決まっているので、エラトステネスの篩で素数を取り出して
+- 和をとる。
+-
+- [結果]
+- 142913828922
+- time:15.869627s
+-}
+sieve :: Int -> [Int]
+sieve n | n < 2 = []
+        | otherwise = 2 : sieve' [3,5..n]
+  where sieve' [] = []
+        sieve' ls@(x:_) | x * x > n = ls
+        sieve' (x:xs) = let rest = filter (\ k -> k `mod` x /= 0) xs in x : (sieve' rest)
+
+solve :: Int -> Integer
+solve = sum . map toInteger . sieve
 
 main :: IO ()
 main = do
--- src/test/Problem/P010Spec.hs
+++ src/test/Problem/P010Spec.hs
@@ -8,6 +8,20 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "sieve" $ do
+    it "returns [] when n = 1" $ do
+      sieve 1 `shouldBe` []
+    it "returns [2] when n = 2" $ do
+      sieve 2 `shouldBe` [2]
+    it "returns [2,3] when n = 3" $ do
+      sieve 3 `shouldBe` [2,3]
+    it "returns [2,3] when n = 4" $ do
+      sieve 4 `shouldBe` [2,3]
+    it "returns [2,3,5] when n = 5" $ do
+      sieve 5 `shouldBe` [2,3,5]
+    it "returns [2,3,5,7,11,13,17,19] when n = 20" $ do
+      sieve 20 `shouldBe` [2, 3, 5, 7, 11, 13, 17, 19]
+
   describe "solve" $ do
     it "returns 17 when n = 10" $ do
       solve 10 `shouldBe` 17

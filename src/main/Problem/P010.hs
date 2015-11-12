1f19fc8132913fb645519dcd67b17f41e30e3e86
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-12 16:26:03 +0900

    [P010] リファクタリング
    結局、最初の方針の方が速かったので復活。primesのメモ化ができている
    からだと思う。
    テストも修正し、スケールもダウン。

--- src/main/Problem/P010.hs
+++ src/main/Problem/P010.hs
@@ -1,4 +1,5 @@
 module Problem.P010 where
+import Common.Arithmetic
 
 {-
 - Summation of primes
@@ -11,27 +12,7 @@ module Problem.P010 where
 -
 - [結果]
 - 142913828922
-- time:36.233071s
--
-- [コミット]
-- ce3ae63
+- time:1.74288s
 -}
-
-{-
-- [方針2]
-- 最大値が決まっているので、エラトステネスの篩で素数を取り出して
-- 和をとる。
--
-- [結果]
-- 142913828922
-- time:15.869627s
--}
-sieve :: Int -> [Int]
-sieve n | n < 2 = []
-        | otherwise = 2 : sieve' [3,5..n]
-  where sieve' [] = []
-        sieve' ls@(x:_) | x * x > n = ls
-        sieve' (x:xs) = let rest = filter (\ k -> k `mod` x /= 0) xs in x : (sieve' rest)
-
-solve :: Int -> Integer
-solve = sum . map toInteger . sieve
+solve :: Integral a => a -> Integer
+solve n = sum $ takeWhile (<= fromIntegral n) (primes :: [Integer])

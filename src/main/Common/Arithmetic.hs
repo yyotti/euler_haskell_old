3440d4ed8e748d4b8340bcf10159d750c0f03a1f
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-12 15:17:30 +0900

    素数関連にSPECIALIZEプラグマを追加

--- src/main/Common/Arithmetic.hs
+++ src/main/Common/Arithmetic.hs
@@ -6,14 +6,20 @@ import Data.List
 fib :: Integral a => [a]
 fib = 0 : 1 : (zipWith (+) fib $ tail fib)
 
+{-# SPECIALIZE primes :: [Int] #-}
+{-# SPECIALIZE primes :: [Integer] #-}
 primes :: Integral a => [a]
 primes = 2 : filter isPrime [3, 5 ..]
 
+{-# SPECIALIZE isPrime :: Int -> Bool #-}
+{-# SPECIALIZE isPrime :: Integer -> Bool #-}
 isPrime :: Integral a => a -> Bool
 isPrime n | n < 2 = False
           | otherwise = all ((/= 0) . mod n) $ ps
   where ps = takeWhile ((<= n) . (^2)) primes
 
+{-# SPECIALIZE primeFactors :: Int -> [Int] #-}
+{-# SPECIALIZE primeFactors :: Integer -> [Integer] #-}
 primeFactors :: Integral a => a -> [a]
 primeFactors n | n < 2 = []
                | otherwise = case find ((== 0) . mod n) ps of

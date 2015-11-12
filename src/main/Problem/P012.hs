9ae541085a5f65f614980ed21ee1c8ddca359d79
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-12 19:53:17 +0900

    [P012] 回答

--- src/main/Problem/P012.hs
+++ src/main/Problem/P012.hs
@@ -1,4 +1,6 @@
 module Problem.P012 where
+import Data.List
+import Common.Arithmetic
 
 {-
 - [Problem name]
@@ -7,8 +9,29 @@ module Problem.P012 where
 
 {-
 - [方針1]
+- 三角数の数列をt(n)とすると、1からnまでの自然数の和なので
+-   t(n) = n(n + 1)/2   (n >= 1)
+- である。
+-
+- 約数の個数m(n)は、nを素因数分解した結果が
+-   n = p1^e1 * p2^e2 * ... * pk^ek   (pkは素数、ekは自然数)
+- とすれば、
+-   m(n) = Π(i = 1 → k) { ei + 1 }
+- で算出できる。
 -
 - [結果]
+- 76576500
+- time:10.200946s
 -}
+
+triangleNumbers :: Integral a => [a]
+triangleNumbers = map t [1..]
+  where t n = n * (n + 1) `div` 2
+
+factorsCount :: Integral a => a -> a
+factorsCount n = product $ map ((+ 1) . fromIntegral . length) $ groupBy (==) $ primeFactors n
+
 solve :: Integral a => a -> Integer
-solve _ = undefined
+solve n = case find ((> n) . factorsCount) triangleNumbers of
+               Just k -> toInteger k
+               _ -> 0
--- src/test/Problem/P012Spec.hs
+++ src/test/Problem/P012Spec.hs
@@ -8,6 +8,36 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "triangleNumbers" $ do
+    it "first 10 terms are [1,3,6,10,15,21,28,36,45,55]" $ do
+      take 10 triangleNumbers `shouldBe` [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+
+  describe "factorsCount" $ do
+    it "returns 1 when n = 1" $ do
+      factorsCount 1 `shouldBe` 1
+    it "returns 2 when n = 2" $ do
+      factorsCount 2 `shouldBe` 2
+    it "returns 2 when n = 3" $ do
+      factorsCount 3 `shouldBe` 2
+    it "returns 3 when n = 4" $ do
+      factorsCount 4 `shouldBe` 3
+    it "returns 2 when n = 5" $ do
+      factorsCount 5 `shouldBe` 2
+    it "returns 4 when n = 6" $ do
+      factorsCount 6 `shouldBe` 4
+    it "returns 2 when n = 7" $ do
+      factorsCount 7 `shouldBe` 2
+    it "returns 4 when n = 8" $ do
+      factorsCount 8 `shouldBe` 4
+    it "returns 3 when n = 9" $ do
+      factorsCount 9 `shouldBe` 3
+    it "returns 4 when n = 10" $ do
+      factorsCount 10 `shouldBe` 4
+    it "returns 4 when n = 15" $ do
+      factorsCount 15 `shouldBe` 4
+    it "returns 6 when n = 28" $ do
+      factorsCount 28 `shouldBe` 6
+
   describe "solve" $ do
     it "returns 28 when n = 5" $ do
       solve 5 `shouldBe` 28

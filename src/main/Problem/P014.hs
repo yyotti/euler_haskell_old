6680ef4a7c0f10fc25b12b68705f5d29f9e35955
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-13 17:13:15 +0900

    [P014] 高速化
    だいぶ速くなったが、もっと速くなる予感はする。

--- src/main/Problem/P014.hs
+++ src/main/Problem/P014.hs
@@ -1,6 +1,8 @@
 module Problem.P014 where
-import Data.List
+import Prelude
 import Data.Ord
+import Data.Map as Map hiding (foldl')
+import Data.List hiding (insert)
 
 {-
 - Longest Collatz sequence
@@ -14,12 +16,48 @@ import Data.Ord
 - [結果]
 - 837799
 - time:28.338644s
+-
+- [コミット]
+- 16b9f53
 -}
 
-chainLength :: Integral a => a -> Int
-chainLength 1 = 1
-chainLength n | even n = 1 + (chainLength $ n `div` 2)
-              | otherwise = 1 + (chainLength $ 3 * n + 1)
+{-
+- [方針2]
+- 問題にある数列に着目する。
+- 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+- この数列は13から始まり10個の項で構成されるが、途中の20から始めた場合は
+- 20 -> 10 -> ...
+- と続き8個の項で数列を構成する。
+- つまり、ある数から始めて生成された数列の部分列は再利用できる。
+-
+- よって下記のアルゴリズムで考える。
+- まず、数字をキーとするマップを用意する。値は、その数字から始めた場合の
+- コラッツ数列の長さとする。マップのキーと値の組を
+-   [k -> v]
+- で表現する。13の場合は10項の数列なので
+-   [13 -> 10]
+- である。
+- ある数について数列の長さを求める際、まずマップに登録されていないかを
+- 調べ、登録されていればその長さを用いる。
+- ある整数nについて、
+-   (nがマップに登録されている場合)
+-     その値を採用する。
+-   (nがマップに登録されていない場合)
+-     定義に従って次の数字mを計算する。n=mとして再帰的に数列の長さを計算し、
+-     その長さ+1をnの値とする。
+-
+- [結果]
+- 837799
+- time:8.890178s
+-}
+
+chainLength :: Integral a => a -> Map a Int -> (Int, Map a Int)
+chainLength n m = case Map.lookup n m of
+                       Just c -> (c, m)
+                       _ -> let (c, m') = chainLength n' m in (c + 1, insert n (c + 1) m')
+  where n' | even n = n `div` 2
+           | otherwise = 3 * n + 1
 
 solve :: Integral a => a -> Integer
-solve n = toInteger $ fst $ maximumBy (comparing snd) $ map (\ i -> (i, chainLength i)) [1..(n - 1)]
+solve n = toInteger $ fst $ maximumBy (comparing snd) $ toList m
+  where m = foldl' (\ m i -> snd $ chainLength i m) (insert 1 1 empty) [2..n]
--- src/test/Problem/P014Spec.hs
+++ src/test/Problem/P014Spec.hs
@@ -2,6 +2,8 @@ module Problem.P014Spec (main, spec) where
 
 import Test.Hspec
 import Problem.P014
+import Data.Map hiding (foldl')
+import Data.List (foldl')
 
 main :: IO ()
 main = hspec spec
@@ -9,26 +11,43 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "chainLength" $ do
+    let m = insert 1 1 empty
+    let addSeq ls = foldl' (\ mm (k, v) -> insert k v mm) m $ zip ls $ reverse [2..(length ls + 1)]
     it "returns 1 when n = 1" $ do
-      chainLength 1 `shouldBe` 1
+      chainLength 1 m `shouldBe` (1, m)
     it "returns 2 when n = 2" $ do
-      chainLength 2 `shouldBe` 2
+      chainLength 2 m `shouldBe` (2, insert 2 2 m)
     it "returns 8 when n = 3" $ do
-      chainLength 3 `shouldBe` 8
+      let m' = insert 3 2 m
+      chainLength 3 m' `shouldBe` (2, m')
+    it "returns 8 when n = 3" $ do
+      let m' = addSeq [3, 10, 5, 16, 8, 4, 2]
+      chainLength 3 m `shouldBe` (8, m')
     it "returns 3 when n = 4" $ do
-      chainLength 4 `shouldBe` 3
+      let m' = addSeq [4, 2]
+      chainLength 4 m `shouldBe` (3, m')
     it "returns 6 when n = 5" $ do
-      chainLength 5 `shouldBe` 6
+      let m' = addSeq [5, 16, 8, 4, 2]
+      chainLength 5 m `shouldBe` (6, m')
     it "returns 9 when n = 6" $ do
-      chainLength 6 `shouldBe` 9
+      let m' = addSeq [6, 3, 10, 5, 16, 8, 4, 2]
+      chainLength 6 m `shouldBe` (9, m')
     it "returns 17 when n = 7" $ do
-      chainLength 7 `shouldBe` 17
+      let m' = addSeq [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2]
+      chainLength 7 m `shouldBe` (17, m')
+    it "returns 17 when n = 7" $ do
+      let m' = addSeq [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2]
+      let m'' = addSeq [13, 40, 20, 10, 5, 16, 8, 4, 2]
+      chainLength 7 m'' `shouldBe` (17, m')
     it "returns 4 when n = 8" $ do
-      chainLength 8 `shouldBe` 4
+      let m' = addSeq [8, 4, 2]
+      chainLength 8 m `shouldBe` (4, m')
     it "returns 20 when n = 9" $ do
-      chainLength 9 `shouldBe` 20
+      let m' = addSeq [9, 28, 14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2]
+      chainLength 9 m `shouldBe` (20, m')
     it "returns 7 when n = 10" $ do
-      chainLength 10 `shouldBe` 7
+      let m' = addSeq [10, 5, 16, 8, 4, 2]
+      chainLength 10 m `shouldBe` (7, m')
 
   describe "solve" $ do
     it "returns 27 when n = 30" $ do

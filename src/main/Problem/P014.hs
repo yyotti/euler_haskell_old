16b9f53be2c14f475f280737e53e8579fd0071e1
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-13 12:47:02 +0900

    [P014] 回答
    予想通り、遅い。再検討する。

--- src/main/Problem/P014.hs
+++ src/main/Problem/P014.hs
@@ -1,4 +1,6 @@
 module Problem.P014 where
+import Data.List
+import Data.Ord
 
 {-
 - Longest Collatz sequence
@@ -7,8 +9,17 @@ module Problem.P014 where
 
 {-
 - [方針1]
+- 問題に書かれている定義に従って処理する。
 -
 - [結果]
+- 837799
+- time:28.338644s
 -}
+
+chainLength :: Integral a => a -> Int
+chainLength 1 = 1
+chainLength n | even n = 1 + (chainLength $ n `div` 2)
+              | otherwise = 1 + (chainLength $ 3 * n + 1)
+
 solve :: Integral a => a -> Integer
-solve _ = undefined
+solve n = toInteger $ fst $ maximumBy (comparing snd) $ map (\ i -> (i, chainLength i)) [1..(n - 1)]
--- src/test/Problem/P014Spec.hs
+++ src/test/Problem/P014Spec.hs
@@ -8,6 +8,28 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "chainLength" $ do
+    it "returns 1 when n = 1" $ do
+      chainLength 1 `shouldBe` 1
+    it "returns 2 when n = 2" $ do
+      chainLength 2 `shouldBe` 2
+    it "returns 8 when n = 3" $ do
+      chainLength 3 `shouldBe` 8
+    it "returns 3 when n = 4" $ do
+      chainLength 4 `shouldBe` 3
+    it "returns 6 when n = 5" $ do
+      chainLength 5 `shouldBe` 6
+    it "returns 9 when n = 6" $ do
+      chainLength 6 `shouldBe` 9
+    it "returns 17 when n = 7" $ do
+      chainLength 7 `shouldBe` 17
+    it "returns 4 when n = 8" $ do
+      chainLength 8 `shouldBe` 4
+    it "returns 20 when n = 9" $ do
+      chainLength 9 `shouldBe` 20
+    it "returns 7 when n = 10" $ do
+      chainLength 10 `shouldBe` 7
+
   describe "solve" $ do
     it "returns 27 when n = 30" $ do
       solve 30 `shouldBe` 27

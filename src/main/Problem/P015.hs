50c519d354ba2bb0252a2b9edd5724f8973dfcbe
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-14 11:23:51 +0900

    [P015] 回答

--- src/main/Problem/P015.hs
+++ src/main/Problem/P015.hs
@@ -7,8 +7,35 @@ module Problem.P015 where
 
 {-
 - [方針1]
+- 格子の左上から右下まで、引き返しなしで行くには、格子の1辺のサイズをnとすると、
+- どのルートも必ず左方向にn、右方向にnの2nステップ進まなければならない。
+- 左に進む場合を.、右に進む場合を^で表すと、各ルートは
+-   ..^.^^..^^. ・・・
+- のように表現できる。
+- つまり、2n個あるステップのうち、どのn個を.(左移動)とするかを選択する個数が
+- そのままルートの数になるので、2nCnで得られる。
+-
+- nCrの計算自体は再帰的にも計算できるが、
+-   nCr = nPr/r!
+- の方が速いと思う。
 -
 - [結果]
+- 137846528820
+- time:0.000078s
 -}
+
+fact :: Integral a => a -> a
+fact 0 = 1
+fact n = n * fact (n - 1)
+
+permutation :: Integral a => a -> a -> a
+permutation _ 0 = 1
+permutation n 1 = n
+permutation n r | n < r = 1
+                | otherwise = n * permutation (n - 1) (r - 1)
+
+combination :: Integral a => a -> a -> a
+combination n r = div (permutation n r) (fact r)
+
 solve :: Integral a => a -> Integer
-solve _ = undefined
+solve n = toInteger $ combination (2 * n) n
--- src/test/Problem/P015Spec.hs
+++ src/test/Problem/P015Spec.hs
@@ -8,8 +8,44 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "fact" $ do
+    it "returns 1 when n = 0" $ do
+      fact 0 `shouldBe` 1
+    it "returns 1 when n = 1" $ do
+      fact 1 `shouldBe` 1
+    it "returns 2 when n = 2" $ do
+      fact 2 `shouldBe` 2
+    it "returns 6 when n = 3" $ do
+      fact 3 `shouldBe` 6
+    it "returns 24 when n = 4" $ do
+      fact 4 `shouldBe` 24
+
+  describe "permutation" $ do
+    it "returns 1 when n < r" $ do
+      permutation 2 3 `shouldBe` 1
+    it "returns 1 when r = 0" $ do
+      permutation 3 0 `shouldBe` 1
+    it "returns 2 when (n, r) = (2, 1)" $ do
+      permutation 2 1 `shouldBe` 2
+    it "returns 120 when (n, r) = (5, 4)" $ do
+      permutation 5 4 `shouldBe` 120
+    it "returns 120 when (n, r) = (6, 3)" $ do
+      permutation 6 3 `shouldBe` 120
+
+  describe "combination" $ do
+    it "returns 0 when n < r" $ do
+      combination 2 3 `shouldBe` 0
+    it "returns 1 when r = 0" $ do
+      combination 3 0 `shouldBe` 1
+    it "returns 2 when (n, r) = (2, 1)" $ do
+      combination 2 1 `shouldBe` 2
+    it "returns 5 when (n, r) = (5, 4)" $ do
+      combination 5 4 `shouldBe` 5
+    it "returns 20 when (n, r) = (6, 3)" $ do
+      combination 6 3 `shouldBe` 20
+
   describe "solve" $ do
     it "returns 6 when n = 2" $ do
       solve 2 `shouldBe` 6
-    it "returns 515 when n = 10" $ do
-      solve 10 `shouldBe` 515
+    it "returns 184756 when n = 10" $ do
+      solve 10 `shouldBe` 184756

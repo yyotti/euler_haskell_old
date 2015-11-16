fffe3e2126f871afaa81276b8e866f7a5f6f237a
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-16 12:11:57 +0900

    [P019] 回答

--- src/main/Problem/P019.hs
+++ src/main/Problem/P019.hs
@@ -7,8 +7,30 @@ module Problem.P019 where
 
 {-
 - [方針1]
+- 1901年1月から2000年12月までの各月の1日について、1900年1月1日からの日数を閏年を
+- 考慮しつつ計算し、その日数が7で割り切れる月をカウントする。
 -
 - [結果]
+- 171
+- time:0.000204s
 -}
+
+isLeapYear :: Int -> Bool
+isLeapYear y = mod y 400 == 0 || mod y 100 /=0 && mod y 4 == 0
+
+dateCount :: Int -> Int -> Int
+dateCount y 2 | isLeapYear y = 29
+              | otherwise = 28
+dateCount _ 4 = 30
+dateCount _ 6 = 30
+dateCount _ 9 = 30
+dateCount _ 11 = 30
+dateCount _ _ = 31
+
 solve :: Integer
-solve = undefined
+solve = countBy ((== 0) . (`mod` 7)) $ scanl (\ z c -> z + c - dateCount1900) dateCount1900 dateCounts
+  where dateCount1900 = sum $ map (dateCount 1900) [1..12]
+        dateCounts = concatMap (\ y -> map (\ m -> dateCount y m) [1..12]) [1901..2000]
+        countBy _ [] = 0
+        countBy f (x:xs) | f x = 1 + countBy f xs
+                         | otherwise = countBy f xs
--- src/test/Problem/P019Spec.hs
+++ src/test/Problem/P019Spec.hs
@@ -8,6 +8,34 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "isLeapYear" $ do
+    it "returns False when y = 1900" $ do
+      isLeapYear 1900 `shouldBe` False
+    it "returns False when y = 1901" $ do
+      isLeapYear 1901 `shouldBe` False
+    it "returns False when y = 1902" $ do
+      isLeapYear 1902 `shouldBe` False
+    it "returns False when y = 1903" $ do
+      isLeapYear 1903 `shouldBe` False
+    it "returns True when y = 1904" $ do
+      isLeapYear 1904 `shouldBe` True
+    it "returns True when y = 2000" $ do
+      isLeapYear 2000 `shouldBe` True
+
+  describe "dateCount" $ do
+    it "returns [31,28,31,30,31,30,31,31,30,31,30,31] when (y,_) = (1900,_)" $ do
+      map (dateCount 1900) [1..12] `shouldBe` [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    it "returns [31,28,31,30,31,30,31,31,30,31,30,31] when (y,_) = (1901,_)" $ do
+      map (dateCount 1901) [1..12] `shouldBe` [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    it "returns [31,28,31,30,31,30,31,31,30,31,30,31] when (y,_) = (1902,_)" $ do
+      map (dateCount 1902) [1..12] `shouldBe` [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    it "returns [31,28,31,30,31,30,31,31,30,31,30,31] when (y,_) = (1903,_)" $ do
+      map (dateCount 1903) [1..12] `shouldBe` [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    it "returns [31,29,31,30,31,30,31,31,30,31,30,31] when (y,_) = (1904,_)" $ do
+      map (dateCount 1904) [1..12] `shouldBe` [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    it "returns [31,29,31,30,31,30,31,31,30,31,30,31] when (y,_) = (2000,_)" $ do
+      map (dateCount 2000) [1..12] `shouldBe` [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
   describe "solve" $ do
     it "is 171" $ do
       solve `shouldBe` 171

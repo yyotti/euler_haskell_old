b36906a709659a5a8a1fa623148c139ce11cab52
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-16 11:00:38 +0900

    [P018] 高速化

--- src/main/Problem/P018.hs
+++ src/main/Problem/P018.hs
@@ -18,15 +18,55 @@ module Problem.P018 where
 - [結果]
 - 1074
 - time:0.022841s
+-
+- [コミット]
+- 0cb804d
 -}
 
-findRoutes :: Integral a => [[a]] -> [[a]]
-findRoutes [] = []
-findRoutes [xs] = [xs]
-findRoutes (xs:ys) = map (top:) $ findRoutes left ++ findRoutes right
-  where top = head xs
-        left = map init ys
-        right = map tail ys
+{-
+- [方針2]
+- 三角形の行数をnとすれば2^(n-1)通りを調べる必要があるので、工夫する。
+-
+- 三角形の中のある数字に注目すると、そこより下のルートで和が最大のルートは、
+-   「左に行くルート」の中で和が最大となるルート
+-   「右に行くルート」の中で和が最大となるルート
+- をそれぞれ算出し、さらに2つのうち大きい方を採用すればよい。
+- 三角形の底辺とその上の行でこのルールを適用すれば、
+-   底辺の数字で隣り合っているものどうしを比較し、大きい方を残す
+- ということになる。問題の小さい三角形では、底辺は[8,5,9,3]であるから、
+-   ・8と5で比較して大きい方を残す・・・8を残す
+-   ・5と9で比較して大きい方を残す・・・9を残す
+-   ・9と3で比較して大きい方を残す・・・9を残す
+- となり、残るのは[8,9,9]である。
+- 結果、三角形は
+-   [3],
+-   [7,4],
+-   [2,4,6],
+-   [8,9,9]
+- となる。
+- 底辺に残ったそれぞれの要素は1つ上の行の要素に対応するので、それぞれ和を
+- とり
+-   [3],
+-   [7,4],
+-   [10,13,15]
+- とできる。これを要素が1つになるまで繰り返せば、残っているのはルートの数字の
+- 和の最大値である。
+-
+- [結果]
+- 1074
+- time:0.000069s
+-}
+
+pairs :: [a] -> [[a]]
+pairs [] = []
+pairs [_] = []
+pairs (x:y:zs) = [x, y] : pairs (y:zs)
+
+findMax :: Integral a => [[a]] -> a
+findMax [[x]] = x
+findMax (xs:ys:zs) = findMax $ zipWith (+) xs' ys : zs
+  where xs' = map maximum $ pairs xs
+findMax _ = 0
 
 solve :: Integral a => [[a]] -> Integer
-solve ls = toInteger $ maximum $ map sum $ findRoutes ls
+solve = toInteger . findMax . reverse
--- src/test/Problem/P018Spec.hs
+++ src/test/Problem/P018Spec.hs
@@ -8,17 +8,29 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "findRoutes" $ do
+  describe "pairs" $ do
     it "returns [] when ls = []" $ do
-      findRoutes [] `shouldBe` []
-    it "returns [[1,2,3]] when ls = [[1,2,3]]" $ do
-      findRoutes [[1, 2, 3]] `shouldBe` [[1, 2, 3]]
-    it "returns [[3,7][3,4]] when ls = [[3],[7,4]]" $ do
-      findRoutes [[3], [7, 4]] `shouldBe` [[3, 7], [3, 4]]
-    it "returns [[3,7,2],[3,7,4],[3,4,4],[3,4,6]] when ls = [[3],[7,4],[2,4,6]]" $ do
-      findRoutes [[3], [7, 4], [2, 4, 6]] `shouldBe` [[3, 7, 2], [3, 7, 4], [3, 4, 4], [3, 4, 6]]
-    it "returns [[3,7,2,8],[3,7,2,5],[3,7,4,5],[3,7,4,9],[3,4,4,5],[3,4,4,9],[3,4,6,9],[3,4,6,3]] when ls = [[3],[7,4],[2,4,6],[8,5,9,3]]" $ do
-      findRoutes [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]] `shouldBe` [[3, 7, 2, 8], [3, 7, 2, 5], [3, 7, 4, 5], [3, 7, 4, 9], [3, 4, 4, 5], [3, 4, 4, 9], [3, 4, 6, 9], [3, 4, 6, 3]]
+      pairs ([] :: [Char]) `shouldBe` []
+    it "returns [] when ls = [1]" $ do
+      pairs [1] `shouldBe` []
+    it "returns [[1,2]] when ls = [1,2]" $ do
+      pairs [1, 2] `shouldBe` [[1, 2]]
+    it "returns [[1,2],[2,3]] when ls = [1,2,3]" $ do
+      pairs [1, 2, 3] `shouldBe` [[1, 2], [2, 3]]
+    it "returns [[1,2],[2,3],[3,4]] when ls = [1,2,3,4]" $ do
+      pairs [1, 2, 3, 4] `shouldBe` [[1, 2], [2, 3], [3, 4]]
+
+  describe "findMax" $ do
+    it "returns 0 when ls = []" $ do
+      findMax [] `shouldBe` 0
+    it "returns 3 when ls = [[3]]" $ do
+      findMax [[3]] `shouldBe` 3
+    it "returns 10 when ls = [[7,4],[3]]" $ do
+      findMax [[7, 4], [3]] `shouldBe` 10
+    it "returns 14 when ls = [[2,4,6],[7,4],[3]]" $ do
+      findMax [[2, 4, 6], [7, 4], [3]] `shouldBe` 14
+    it "returns 23 when ls = [[8,5,9,3],[2,4,6],[7,4],[3]]" $ do
+      findMax [[8, 5, 9, 3], [2, 4, 6], [7, 4], [3]] `shouldBe` 23
 
   describe "solve" $ do
     it "returns 23 when t = [[3],[7,4],[2,4,6],[8,5,9,3]]" $ do

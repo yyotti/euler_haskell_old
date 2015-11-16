e06f441ea762273dd14887af41e5b26924a60521
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-16 14:27:05 +0900

    [P020] 準備

---
+++ src/main/Problem/P020.hs
@@ -0,0 +1,14 @@
+module Problem.P020 where
+
+{-
+- Factorial digit sum
+- https://projecteuler.net/problem=20
+-}
+
+{-
+- [方針1]
+-
+- [結果]
+-}
+solve :: Integral a => a -> Integer
+solve _ = undefined
---
+++ src/test/Problem/P020Spec.hs
@@ -0,0 +1,15 @@
+module Problem.P020Spec (main, spec) where
+
+import Test.Hspec
+import Problem.P020
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "solve" $ do
+    it "returns 27 when n = 10" $ do
+      solve 10 `shouldBe` 27
+    it "returns 30 when n = 20" $ do
+      solve 20 `shouldBe` 30

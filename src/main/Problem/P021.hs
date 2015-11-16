3f57693f9b246e83d68d07129da44542fc99804c
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-16 15:28:15 +0900

    [P021] 回答

--- src/main/Problem/P021.hs
+++ src/main/Problem/P021.hs
@@ -1,4 +1,6 @@
 module Problem.P021 where
+import Data.List
+import Common.Arithmetic
 
 {-
 - Amicable numbers
@@ -7,8 +9,36 @@ module Problem.P021 where
 
 {-
 - [方針1]
+- 指定された数未満のd(n)を求め、それをmとする。
+-   d(m) = n  (m ≠ n)
+- である場合をカウントしていく。
+- ただし重複が考えられるので、最後に2で割らなければならない。
+- また、場合によっては d(n) が最大値を超える可能性があるので注意する。
+-
+- nが
+-   n = p1^e1 * p2^e2 * ... * pk^ek  (pk は素数、ekは自然数)
+- と因数分解できるとき、nの約数の和は
+-   (1 + p1 + p1^2 + ... + p1^e1)*(1 + p2 + p2^2 + ... + p2^e2)
+- で表すことができる。
 -
 - [結果]
+- 31626
+- time:0.418841s
 -}
+
+primeFactorsCount :: Integral a => a -> [(a, Int)]
+primeFactorsCount = map toTuple . group . primeFactors
+  where toTuple ls = (head ls, length ls)
+
+factorsSum :: Integral a => a -> a
+factorsSum = product . map s . primeFactorsCount
+  where s (p, e) = sum $ map (p^) [0..e]
+
+amicableNum :: Integral a => a -> a -> [a]
+amicableNum m n | d2 == n && d1 < m && d1 /= n = [n, d1]
+                | otherwise = []
+  where d1 = factorsSum n - n
+        d2 = factorsSum d1 - d1
+
 solve :: Integral a => a -> Integer
-solve _ = undefined
+solve n = toInteger $ (sum . concatMap (amicableNum n)) [1..n-1] `div` 2
--- src/test/Problem/P021Spec.hs
+++ src/test/Problem/P021Spec.hs
@@ -8,6 +8,68 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "primeFactorsCount" $ do
+    it "returns [] when n = 1" $ do
+      primeFactorsCount 1 `shouldBe` []
+    it "returns [(2,1)] when n = 2" $ do
+      primeFactorsCount 2 `shouldBe` [(2, 1)]
+    it "returns [(3,1)] when n = 3" $ do
+      primeFactorsCount 3 `shouldBe` [(3, 1)]
+    it "returns [(2,2)] when n = 4" $ do
+      primeFactorsCount 4 `shouldBe` [(2, 2)]
+    it "returns [(5,1)] when n = 5" $ do
+      primeFactorsCount 5 `shouldBe` [(5, 1)]
+    it "returns [(2,1),(3,1)] when n = 6" $ do
+      primeFactorsCount 6 `shouldBe` [(2, 1), (3, 1)]
+    it "returns [(7,1)] when n = 7" $ do
+      primeFactorsCount 7 `shouldBe` [(7, 1)]
+    it "returns [(2,3)] when n = 8" $ do
+      primeFactorsCount 8 `shouldBe` [(2, 3)]
+    it "returns [(3,2)] when n = 9" $ do
+      primeFactorsCount 9 `shouldBe` [(3, 2)]
+    it "returns [(2,1),(5,1)] when n = 10" $ do
+      primeFactorsCount 10 `shouldBe` [(2, 1), (5, 1)]
+    it "returns [(11,1)] when n = 11" $ do
+      primeFactorsCount 11 `shouldBe` [(11, 1)]
+
+  describe "factorsSum" $ do
+    it "returns 1 when n = 1" $ do
+      factorsSum 1 `shouldBe` 1
+    it "returns 3 when n = 2" $ do
+      factorsSum 2 `shouldBe` 3
+    it "returns 4 when n = 3" $ do
+      factorsSum 3 `shouldBe` 4
+    it "returns 7 when n = 4" $ do
+      factorsSum 4 `shouldBe` 7
+    it "returns 6 when n = 5" $ do
+      factorsSum 5 `shouldBe` 6
+    it "returns 12 when n = 6" $ do
+      factorsSum 6 `shouldBe` 12
+    it "returns 8 when n = 7" $ do
+      factorsSum 7 `shouldBe` 8
+    it "returns 15 when n = 8" $ do
+      factorsSum 8 `shouldBe` 15
+    it "returns 13 when n = 9" $ do
+      factorsSum 9 `shouldBe` 13
+    it "returns 18 when n = 10" $ do
+      factorsSum 10 `shouldBe` 18
+    it "returns 12 when n = 11" $ do
+      factorsSum 11 `shouldBe` 12
+
+  describe "amicableNum" $ do
+    it "returns [1,0] when (m,n) = (1,1)" $ do
+      amicableNum 1 1 `shouldBe` [1,0]
+    it "returns [1,0] when (m,n) = (10,1)" $ do
+      amicableNum 10 1 `shouldBe` [1,0]
+    it "returns [] when (m,n) = (10,2)" $ do
+      amicableNum 10 2 `shouldBe` []
+    it "returns [] when (m,n) = (10,6)" $ do
+      amicableNum 10 6 `shouldBe` []
+    it "returns [220,284] when (m,n) = (500,220)" $ do
+      amicableNum 500 220 `shouldBe` [220, 284]
+    it "returns [] when (m,n) = (250,220)" $ do
+      amicableNum 250 220 `shouldBe` []
+
   describe "solve" $ do
     it "returns 504 when n = 1000" $ do
       solve 1000 `shouldBe` 504

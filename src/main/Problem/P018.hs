0cb804d20df3cbdd5ea2da35c0af9f7723cf8a82
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-16 10:12:52 +0900

    [P018] 回答

--- src/main/Problem/P018.hs
+++ src/main/Problem/P018.hs
@@ -7,8 +7,26 @@ module Problem.P018 where
 
 {-
 - [方針1]
+- 問題の注釈を信じるなら、15段で全パターンを調べても16384通りしかないらしいので、
+- まずはストレートに、全てのルートを検出して解いてみる。
+-
+- 全てのルートを検出するためには、頂点から辿って
+-   左に行くルート・・・問題の小さい三角形なら 3 -> 7 と行くルート
+-   右に行くルート・・・問題の小さい三角形なら 3 -> 4 と行くルート
+- をそれぞれ再帰的に求め、それぞれのルートに対して頂点の3を先頭に加えてやればよい。
 -
 - [結果]
+- 1074
+- time:0.022841s
 -}
+
+findRoutes :: Integral a => [[a]] -> [[a]]
+findRoutes [] = []
+findRoutes [xs] = [xs]
+findRoutes (xs:ys) = map (top:) $ findRoutes left ++ findRoutes right
+  where top = head xs
+        left = map init ys
+        right = map tail ys
+
 solve :: Integral a => [[a]] -> Integer
-solve _ = undefined
+solve ls = toInteger $ maximum $ map sum $ findRoutes ls
--- src/test/Problem/P018Spec.hs
+++ src/test/Problem/P018Spec.hs
@@ -8,6 +8,18 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "findRoutes" $ do
+    it "returns [] when ls = []" $ do
+      findRoutes [] `shouldBe` []
+    it "returns [[1,2,3]] when ls = [[1,2,3]]" $ do
+      findRoutes [[1, 2, 3]] `shouldBe` [[1, 2, 3]]
+    it "returns [[3,7][3,4]] when ls = [[3],[7,4]]" $ do
+      findRoutes [[3], [7, 4]] `shouldBe` [[3, 7], [3, 4]]
+    it "returns [[3,7,2],[3,7,4],[3,4,4],[3,4,6]] when ls = [[3],[7,4],[2,4,6]]" $ do
+      findRoutes [[3], [7, 4], [2, 4, 6]] `shouldBe` [[3, 7, 2], [3, 7, 4], [3, 4, 4], [3, 4, 6]]
+    it "returns [[3,7,2,8],[3,7,2,5],[3,7,4,5],[3,7,4,9],[3,4,4,5],[3,4,4,9],[3,4,6,9],[3,4,6,3]] when ls = [[3],[7,4],[2,4,6],[8,5,9,3]]" $ do
+      findRoutes [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]] `shouldBe` [[3, 7, 2, 8], [3, 7, 2, 5], [3, 7, 4, 5], [3, 7, 4, 9], [3, 4, 4, 5], [3, 4, 4, 9], [3, 4, 6, 9], [3, 4, 6, 3]]
+
   describe "solve" $ do
     it "returns 23 when t = [[3],[7,4],[2,4,6],[8,5,9,3]]" $ do
       let t = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]

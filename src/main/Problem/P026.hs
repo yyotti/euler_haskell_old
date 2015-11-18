167180c00cc4f3048d3240c1eac18c2b15c1db09
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-18 20:04:30 +0900

    [P026] 回答

--- src/main/Problem/P026.hs
+++ src/main/Problem/P026.hs
@@ -1,4 +1,6 @@
 module Problem.P026 where
+import Data.List
+import Data.Ord
 
 {-
 - Reciprocal cycles
@@ -7,8 +9,28 @@ module Problem.P026 where
 
 {-
 - [方針1]
+- 1/dを0.a1a2a3...と表記する。akは小数点以下の各桁の数字を表す。
+-
+- akの具体的な値を求めるために、筆算と同じ手順を繰り返す。つまり
+-   1. 分母を10倍してdで割った商をp、余りをqとすると、a1=pである。
+-   2. qを10倍してdで割った商をp'、余りをq'とすると、a2=p'である。
+-   3. q=q'として2の手順を繰り返すと、a3以降も求められる。
+-   4. q=0となった場合、割り切れたのでそこで終了する。循環節は
+-      存在しない。(長さは0)
+-   5. 3を繰り返した結果、一度計算したqが再び出現したら、最初にqが
+-      出現した位置からそこまでが循環節である。
 -
 - [結果]
+- 983
+- time:2.777817s
 -}
+
+recurringCycle :: Integral a => a -> [a]
+recurringCycle d = recurringCycle' 1 [] []
+  where recurringCycle' q ps qs | q == 0 = []
+                                | elem q qs = map fst $ dropWhile ((/= q) . snd) $ reverse $ zip ps qs
+                                | otherwise = recurringCycle' q' (p:ps) (q:qs)
+          where (p, q') = (q * 10) `divMod` d
+
 solve :: Integral a => a -> Integer
-solve _ = undefined
+solve n = toInteger $ maximumBy (comparing (length . recurringCycle)) [1..n]
--- src/test/Problem/P026Spec.hs
+++ src/test/Problem/P026Spec.hs
@@ -8,6 +8,28 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "recurringCycle" $ do
+    it "returns [] when d = 1" $ do
+      recurringCycle 1 `shouldBe` []
+    it "returns [] when d = 2" $ do
+      recurringCycle 2 `shouldBe` []
+    it "returns [3] when d = 3" $ do
+      recurringCycle 3 `shouldBe` [3]
+    it "returns [] when d = 4" $ do
+      recurringCycle 4 `shouldBe` []
+    it "returns [] when d = 5" $ do
+      recurringCycle 5 `shouldBe` []
+    it "returns [6] when d = 6" $ do
+      recurringCycle 6 `shouldBe` [6]
+    it "returns [1,4,2,8,5,7] when d = 7" $ do
+      recurringCycle 7 `shouldBe` [1,4,2,8,5,7]
+    it "returns [] when d = 8" $ do
+      recurringCycle 8 `shouldBe` []
+    it "returns [1] when d = 9" $ do
+      recurringCycle 9 `shouldBe` [1]
+    it "returns [] when d = 10" $ do
+      recurringCycle 10 `shouldBe` []
+
   describe "solve" $ do
     it "returns 7 when n = 10" $ do
       solve 10 `shouldBe` 7

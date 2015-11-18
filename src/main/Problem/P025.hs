0ef95172d5f9d61be3a79f12e03316d7c842e05c
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-18 14:39:41 +0900

    [P025] 回答

--- src/main/Problem/P025.hs
+++ src/main/Problem/P025.hs
@@ -1,4 +1,7 @@
 module Problem.P025 where
+import Common.Arithmetic
+import Data.List
+import Data.Maybe
 
 {-
 - 1000-digit Fibonacci number
@@ -7,8 +10,11 @@ module Problem.P025 where
 
 {-
 - [方針1]
+- フィボナッチ数列を作って普通にやる
 -
 - [結果]
+- 4782
+- time:0.001273s
 -}
 solve :: Integral a => a -> Integer
-solve _ = undefined
+solve n = toInteger $ fromJust $ findIndex (\ f -> f >= 10^(n - 1)) fib

7a26142ae630bb4f068587e9a70c2bda75e9b904
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-18 12:57:02 +0900

    [P024] 高速化

--- src/main/Problem/P024.hs
+++ src/main/Problem/P024.hs
@@ -1,5 +1,6 @@
 module Problem.P024 where
-import Data.List hiding (permutations)
+import Data.List
+import Common.Arithmetic
 
 {-
 - Lexicographic permutations
@@ -16,11 +17,46 @@ import Data.List hiding (permutations)
 - time:0.51052s
 -}
 
-permutations :: Eq a => [a] -> [[a]]
-permutations [] = []
-permutations [x] = [[x]]
-permutations ls = concatMap (\ x -> map (x:) $ permutations $ delete x ls) ls
+{-
+- [方針2]
+- permutationsで全ての順列を列挙するのは時間がかかるので、i番目をピンポイントに
+- 生成する方法を考える。
+-
+- 順列を生成する元のリストを[a0,a1,...,ak]とする。ただしa0からakは辞書順に並んで
+- いるものとする。(つまり a0 < a1 < ... < ak)
+- このとき、a0を先頭に置く並べ方は k! 通りある。もし
+-   k! < i
+- であるなら、a0を先頭にした並べ方の中にi番目は無いことが分かる。逆に
+-   k! >= i
+- であるなら、a0を先頭にした並べ方の中にi番目が存在する。
+- 後者である場合、先頭がa0で確定するため、残りの[a1,...,ak]で順列を作り、そのi番目
+- を選択すればよいことになる。
+- 前者であるなら、a1からakを先頭にした並べ方もa0のときと同じように k! 通りあるわけ
+- なので、
+-   i / k!  (小数点以下は切り捨て)
+- で何番目の要素を先頭に置くかが分かる。その後、残りの要素で順列を作り、その中の
+-   i mod k!
+- 番目の要素をとればよい。
+-
+- 前者・後者で分けて考えたが、商と余りを求める操作はk!とiの大小関係によらず同じ結果
+- となるので、
+-   リストの i/k! 番目を先頭とする
+-   残りの要素で順列を作りそれの i mod k! 番目をとる
+- の操作を再帰的に繰り返せばよい。
+-
+- [結果]
+- 2783915460
+- time:0.000084s
+-}
 
-solve :: (Show a, Integral a) => [a] -> Int -> Integer
-solve ns i = toInteger $ toNum $ (permutations ns) !! (i - 1)
+permutation :: Eq a => [a] -> Int -> [a]
+permutation [] _ = []
+permutation ls 0 = ls
+permutation ls i = h : (permutation rs (i `mod` f))
+  where f = (fact . length . tail) ls
+        h = ls !! (i `div` f)
+        rs = delete h ls
+
+solve :: Integral a => [a] -> Int -> Integer
+solve ns i = toInteger $ toNum $ permutation ns (i - 1)
   where toNum = foldl' (\ n k -> n * 10 + k) 0
--- src/test/Problem/P024Spec.hs
+++ src/test/Problem/P024Spec.hs
@@ -8,9 +8,19 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "permutations" $ do
-    it "returns [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]] when ls = [0,1,2]" $ do
-      permutations [0, 1, 2] `shouldBe` [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]]
+  describe "permutation" $ do
+    it "returns [0,1,2] when (ls,i) = ([0,1,2],0)" $ do
+      permutation [0, 1, 2] 0 `shouldBe` [0, 1, 2]
+    it "returns [0,2,1] when (ls,i) = ([0,1,2],1)" $ do
+      permutation [0, 1, 2] 1 `shouldBe` [0, 2, 1]
+    it "returns [1,0,2] when (ls,i) = ([0,1,2],2)" $ do
+      permutation [0, 1, 2] 2 `shouldBe` [1, 0, 2]
+    it "returns [1,2,0] when (ls,i) = ([0,1,2],3)" $ do
+      permutation [0, 1, 2] 3 `shouldBe` [1, 2, 0]
+    it "returns [2,0,1] when (ls,i) = ([0,1,2],4)" $ do
+      permutation [0, 1, 2] 4 `shouldBe` [2, 0, 1]
+    it "returns [2,1,0] when (ls,i) = ([0,1,2],5)" $ do
+      permutation [0, 1, 2] 5 `shouldBe` [2, 1, 0]
 
   describe "solve" $ do
     it "returns 12 when (ns,i) = ([0,1,2],1)" $ do

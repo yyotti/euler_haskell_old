68ad10bffd64f613a77b694c1b66be2889c1721c
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-20 20:08:06 +0900

    [P030] 回答

--- src/main/Problem/P030.hs
+++ src/main/Problem/P030.hs
@@ -1,4 +1,5 @@
 module Problem.P030 where
+import Common.Util
 
 {-
 - Digit fifth powers
@@ -7,8 +8,44 @@ module Problem.P030 where
 
 {-
 - [方針1]
+- まず範囲を決める。
+-
+- k桁の整数aのi桁目の数字をa(i) (0 <= a(i) <= 9, 0 <= i <= k-1) とする。
+- このときaは
+-   a = a(k-1)*10^(k-1) + a(k-2)*10^(k-2) + ... + a(1)*10^1 + a(0)*10^0
+- と表せる。
+-
+- aの各桁をn乗した数の和をS(a, n)とすれば、
+-   S(a, n) = a(k-1)^n + a(k-2)^n + ... + a1^n + a0^n
+-           = Σ(i = 0 -> k-1) { ai^n }
+- である。
+- S(n)が最大になるのは全ての桁が9の場合なので、
+-   max S(a, n) = Σ(i = 0 -> k-1) { 9^n }
+-               = k*9^n
+- となる。この桁数をmとすると、m < kの場合は条件を満たすことがない。
+- よって、
+-   m = log10(max S(a, n))
+-     = log10(k*9^n) >= k
+- つまり
+-   k*9^n >= 10^k
+- を満たせばよい。
+-
+- S(n)の最小値について考えると、1^4は含まないということなので、
+- nが1桁では条件を満たす数が存在しない。よって k >= 2 で考えれば
+- よい。
 -
 - [結果]
+- 443839
+- time:0.745951s
 -}
+
+findMax :: Integral a => a -> Int
+findMax n = length $ takeWhile (\ i -> i * 9^n >= 10^(i - 1)) [1..]
+
+findDigitPowers :: Integral a => a -> [a]
+findDigitPowers n = filter (\ i -> digitSum i == i) [10..10^k - 1]
+  where k = findMax n
+        digitSum = sum . map (^n) . digits
+
 solve :: Int -> Integer
-solve _ = undefined
+solve = toInteger . sum . findDigitPowers
--- src/test/Problem/P030Spec.hs
+++ src/test/Problem/P030Spec.hs
@@ -8,6 +8,28 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "findMax" $ do
+    it "returns 2 when n = 1" $ do
+      findMax 1 `shouldBe` 2
+    it "returns 3 when n = 2" $ do
+      findMax 2 `shouldBe` 3
+    it "returns 4 when n = 3" $ do
+      findMax 3 `shouldBe` 4
+    it "returns 5 when n = 4" $ do
+      findMax 4 `shouldBe` 5
+    it "returns 6 when n = 5" $ do
+      findMax 5 `shouldBe` 6
+
+  describe "findDigitPowers" $ do
+    it "returns [] when n = 1" $ do
+      findDigitPowers 1 `shouldBe` []
+    it "returns [] when n = 2" $ do
+      findDigitPowers 2 `shouldBe` []
+    it "returns [153,370,371,407] when n = 3" $ do
+      findDigitPowers 3 `shouldBe` [153, 370, 371, 407]
+
   describe "solve" $ do
+    it "returns 1301 when n = 3" $ do
+      solve 3 `shouldBe` 1301
     it "returns 19316 when n = 4" $ do
       solve 4 `shouldBe` 19316

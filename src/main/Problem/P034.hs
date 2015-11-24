6485e7ff5810bbe9dfc2ebf3a554ce5b520d52f2
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-24 19:48:51 +0900

    [P034] 高速化

--- src/main/Problem/P034.hs
+++ src/main/Problem/P034.hs
@@ -1,9 +1,10 @@
 module Problem.P034 where
 import Common.Util
 import Common.Arithmetic
+import Data.List
 
 {-
-- Digit fractorials
+- Digit factorials
 - https://projecteuler.net/problem=34
 -}
 
@@ -27,15 +28,47 @@ import Common.Arithmetic
 - [結果]
 - 40730
 - time:2.190381s
+-
+- [コミット]
+- f17797c
+-}
+
+{-
+- [方針2]
+- 桁ごとの階乗の和は、整数nの各桁の並び順を変更しても変わらないので、
+- 各桁の階乗和と一致するnが見つかったら、nの各桁の数字を並べ変えた
+- 数は対象から除外してよい。
+-
+- 逆に考えれば、0から9までの数字の階乗を組み合わせて和をとることで
+- 生成できる数字を抽出すればよいことになる。
+- この組み合わせを
+-   a(k-1)はak以上の数字を使えばよい
+- というルールで生成していく。
+-
+- なお、組み合わせを作る際に、0と1は区別する必要がない。
+- 0! = 1、1! = 1であるから、[0,0,0]も[1,1,1]も、階乗和をとればともに
+- 3となるため、重複を避けるために組み合わせは0を含めずに作る。
+-
+- [結果]
+- 40730
+- time:0.523135s
 -}
 
 facts :: [Int]
 facts = map fact [0..9]
 
-digitFactSum :: Integral a => a -> Int
-digitFactSum = sum . map (facts !!) . digits
+nums :: Int -> [[Int]]
+nums k | k < 1 = []
+       | k == 1 = map (:[]) [3..9]
+       | otherwise = concatMap (\ i -> map (i:) $ nums' i (k-1)) [1..9]
+  where nums' m 1 = map (:[]) [m..9]
+        nums' m k = concatMap (\ i -> map (i:) $ nums' i (k-1)) [m..9]
+
+factSum :: [Int] -> Int
+factSum = sum . map (facts !!)
+
+isDigitFactorial :: Int -> Bool
+isDigitFactorial n = n == factSum (digits n)
 
 solve :: Integer
-solve = toInteger $ sum $ filter (\ n -> digitFactSum n == n) nums
-  where mx = 7 * fact 9
-        nums = [3..mx]
+solve = toInteger $ sum $ filter isDigitFactorial $ dropWhile (<= 2) $ nub $ sort $ map factSum $ concatMap nums [1..7]

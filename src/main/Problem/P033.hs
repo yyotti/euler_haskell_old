6c2bed5978c60aa06f4a3087b0540ec565dfbb8c
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-24 09:58:14 +0900

    [P033] 回答

--- src/main/Problem/P033.hs
+++ src/main/Problem/P033.hs
@@ -1,4 +1,6 @@
 module Problem.P033 where
+import Common.Util
+import Data.List
 
 {-
 - Digit cancelling fractions
@@ -7,8 +9,41 @@ module Problem.P033 where
 
 {-
 - [方針1]
+- 分子・分母がともに2桁になるものを探すので、分子・分母ともに10以上100未満で
+- 探すことにする。
+- ただし、30/50のような形、つまり分母・分子ともに10の倍数になっているものは
+- 自明な例として除く。また1より小さい数なので、分子が分母より小さくなければ
+- ならない。
 -
 - [結果]
+- 100
+- time:0.002376s
 -}
+
+type Frac = (Int, Int)
+
+lct :: Frac -> Frac
+lct (n, d) = (n `div` g, d `div` g)
+  where g = gcd n d
+
+incorrectLct :: Frac -> Frac
+incorrectLct (n, d) | n < 10 || n > 99 || d < 10 || n > 99 = (n, d)
+incorrectLct (n, d) = cancel (n1, n2) (d1, d2)
+  where [n1, n2] = digits n
+        [d1, d2] = digits d
+        cancel (n1, n2) (d1, d2) | n1 == d1 = (n2, d2)
+                                 | n1 == d2 = (n2, d1)
+                                 | n2 == d1 = (n1, d2)
+                                 | n2 == d2 = (n1, d1)
+                                 | otherwise = (n, d)
+
+fractions :: [Frac]
+fractions = filter (\ (n, d) -> n `mod` 10 /= 0 || d `mod` 10 /= 0) pairs
+  where pairs = concatMap (\ d -> map (\ n -> (n, d)) [10..d-1]) [11..99]
+
+isDcf :: Frac -> Bool
+isDcf f = f /= f' && lct f == lct f'
+  where f' = incorrectLct f
+
 solve :: Integer
-solve = undefined
+solve = toInteger $ snd $ lct $ foldl' (\ (n1, d1) (n2, d2) -> (n1 * n2, d1 * d2)) (1, 1) $ filter isDcf fractions
--- src/test/Problem/P033Spec.hs
+++ src/test/Problem/P033Spec.hs
@@ -8,6 +8,44 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "lct" $ do
+    it "returns (2,1) when (n,d) = (2,1)" $ do
+      lct (2, 1) `shouldBe` (2, 1)
+    it "returns (3,2) when (n,d) = (3,2)" $ do
+      lct (3, 2) `shouldBe` (3, 2)
+    it "returns (1,2) when (n,d) = (1,2)" $ do
+      lct (1, 2) `shouldBe` (1, 2)
+    it "returns (1,2) when (n,d) = (2,4)" $ do
+      lct (2, 4) `shouldBe` (1, 2)
+    it "returns (2,3) when (n,d) = (6,9)" $ do
+      lct (6, 9) `shouldBe` (2, 3)
+
+  describe "incorrectLct" $ do
+    it "returns (2,23) when (n,d) = (2,23)" $ do
+      incorrectLct (2, 23) `shouldBe` (2, 23)
+    it "returns (20,11) when (n,d) = (20,11)" $ do
+      incorrectLct (20, 11) `shouldBe` (20, 11)
+    it "returns (3,2) when (n,d) = (13,21)" $ do
+      incorrectLct (13, 21) `shouldBe` (3, 2)
+    it "returns (1,2) when (n,d) = (41,42)" $ do
+      incorrectLct (41, 42) `shouldBe` (1, 2)
+    it "returns (2,4) when (n,d) = (25,45)" $ do
+      incorrectLct (25, 45) `shouldBe` (2, 4)
+    it "returns (6,9) when (n,d) = (67,79)" $ do
+      incorrectLct (67, 79) `shouldBe` (6, 9)
+
+  describe "fractions" $ do
+    it "first 10 terms are [(10,11),(10,12),(11,12),(10,13),(11,13),(12,13),(10,14),(11,14),(12,14),(13,14)]" $ do
+      take 10 fractions `shouldBe` [(10, 11), (10, 12), (11, 12), (10, 13), (11, 13), (12, 13), (10, 14), (11, 14), (12, 14), (13, 14)]
+
+  describe "isDcf" $ do
+    it "returns False when f = (20,11)" $ do
+      isDcf (20, 11) `shouldBe` False
+    it "returns True when f = (49,98)" $ do
+      isDcf (49, 98) `shouldBe` True
+    it "returns False when f = (13,21)" $ do
+      isDcf (13, 21) `shouldBe` False
+
   describe "solve" $ do
     it "is 100" $ do
       solve `shouldBe` 100

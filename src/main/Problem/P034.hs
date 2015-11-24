f17797c9095d0814ba2d20033c9a952ff78bc435
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-24 14:56:05 +0900

    [P034] 回答

--- src/main/Problem/P034.hs
+++ src/main/Problem/P034.hs
@@ -1,4 +1,6 @@
 module Problem.P034 where
+import Common.Util
+import Common.Arithmetic
 
 {-
 - Digit fractorials
@@ -7,8 +9,33 @@ module Problem.P034 where
 
 {-
 - [方針1]
+- k桁の整数nについて、各桁を ak,a(k-1),...,a1 とする。
+- このとき各桁の階乗和Sは
+-   S = (ak)! + (a(k-1))! + ... + (a1)!
+- となる。
+- 各桁の数字は全て0以上9以下なので、Sの最大値Smaxは
+-   Smax = k*9! = 362880k
+- である。
+-
+- k = 6のとき Smax = 2177280 (7桁)
+- k = 7のとき Smax = 2540160 (7桁)
+- k = 8のとき Smax = 2903040 (7桁)
+- となるから、nが8桁になると常に n > S となってしまい、一致することは
+- ありえない。よって n <= 2540160 の範囲で探せばよいが、問題に 1! = 1 と
+- 2! = 2 は除外するとあるので、 3 <= n <= 2540160 の範囲で探す。
 -
 - [結果]
+- 40730
+- time:2.190381s
 -}
+
+facts :: [Int]
+facts = map fact [0..9]
+
+digitFactSum :: Integral a => a -> Int
+digitFactSum = sum . map (facts !!) . digits
+
 solve :: Integer
-solve = undefined
+solve = toInteger $ sum $ filter (\ n -> digitFactSum n == n) nums
+  where mx = 7 * fact 9
+        nums = [3..mx]
--- src/test/Problem/P034Spec.hs
+++ src/test/Problem/P034Spec.hs
@@ -8,6 +8,16 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "solve" $ do
-    it "is 40730" $ do
-      solve `shouldBe` 40730
+  describe "facts" $ do
+    it "is [1,1,2,6,24,120,720,5040,40320,362880]" $ do
+      facts `shouldBe` [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+
+  describe "digitFactSum" $ do
+    it "returns 6 when n = 3" $ do
+      digitFactSum 3 `shouldBe` 6
+    it "returns 2 when n = 10" $ do
+      digitFactSum 10 `shouldBe` 2
+    it "returns 2 when n = 11" $ do
+      digitFactSum 11 `shouldBe` 2
+    it "returns 145 when n = 145" $ do
+      digitFactSum 145 `shouldBe` 145

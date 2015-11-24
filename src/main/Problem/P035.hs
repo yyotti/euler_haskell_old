180b554bc59a11adff7e345883d90fa7bf429a49
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-24 20:15:28 +0900

    [P035] 回答

--- src/main/Problem/P035.hs
+++ src/main/Problem/P035.hs
@@ -1,4 +1,6 @@
 module Problem.P035 where
+import Common.Arithmetic
+import Common.Util
 
 {-
 - Circular primes
@@ -7,8 +9,19 @@ module Problem.P035 where
 
 {-
 - [方針1]
+- 100万未満の素数を列挙して、巡回素数かどうか順に調べていく。
 -
 - [結果]
+- 55
+- time:0.755195s
 -}
+
+cycles :: Integral a => a -> [a]
+cycles n = cycles' n e
+  where e = length $ digits n
+        cycles' _ 0 = []
+        cycles' n k = n : cycles' (m * 10 + d) (k-1)
+          where (d, m) = n `divMod` (10^(e-1))
+
 solve :: Int -> Integer
-solve _ = undefined
+solve = toInteger . length . filter (all isPrime . cycles) . flip takeWhile primes . (>=)
--- src/test/Problem/P035Spec.hs
+++ src/test/Problem/P035Spec.hs
@@ -8,6 +8,16 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "cycles" $ do
+    it "returns [1] when n = 1" $ do
+      cycles 1 `shouldBe` [1]
+    it "returns [11,11] when n = 11" $ do
+      cycles 11 `shouldBe` [11, 11]
+    it "returns [12,21] when n = 12" $ do
+      cycles 12 `shouldBe` [12, 21]
+    it "returns [123,231,312] when n = 123" $ do
+      cycles 123 `shouldBe` [123, 231, 312]
+
   describe "solve" $ do
     it "returns 13 when n = 100" $ do
       solve 100 `shouldBe` 13

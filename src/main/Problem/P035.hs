8659a4835a49e1944c49cc080d1947ce77c606f3
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-25 17:02:02 +0900

    [P035] 高速化

--- src/main/Problem/P035.hs
+++ src/main/Problem/P035.hs
@@ -1,6 +1,7 @@
 module Problem.P035 where
 import Common.Arithmetic
 import Common.Util
+import Control.Monad
 
 {-
 - Circular primes
@@ -16,6 +17,22 @@ import Common.Util
 - time:0.755195s
 -}
 
+{-
+- [方針2]
+- nが巡回素数であるためには、下記の条件を満たさなければならない。
+-   1. nは素数である
+-   2. 桁を回転させたときに得られる数が素数でなければならない。
+- 2の条件を満たすためには、nの全ての桁の数字が5以外の奇数でなければならない。
+- 偶数や5を含むと、桁を回転させていけばいつかそれが1の位になり、明らかに
+- 素数ではなくなる。
+-
+- ただし、1桁の素数は巡回素数であるから、2と5はカウントしてよい。
+-
+- [結果]
+- 55
+- time:0.409705s
+-}
+
 cycles :: Integral a => a -> [a]
 cycles n = cycles' n e
   where e = length $ digits n
@@ -23,5 +40,12 @@ cycles n = cycles' n e
         cycles' n k = n : cycles' (m * 10 + d) (k-1)
           where (d, m) = n `divMod` (10^(e-1))
 
+allDigitsOdd :: Integral a => a -> Bool
+allDigitsOdd 0 = True
+allDigitsOdd n = allDigitsOdd m && odd r && r /= 5
+  where (m, r) = n `divMod` 10
+
 solve :: Int -> Integer
-solve = toInteger . length . filter (all isPrime . cycles) . flip takeWhile primes . (>=)
+solve n = toInteger $ length lowers + length highers
+  where (lowers, primes') = span (< 10 `min` n) primes
+        highers = filter (ap ((&&) . allDigitsOdd) (all isPrime . cycles)) $ takeWhile (<= n) primes'
--- src/test/Problem/P035Spec.hs
+++ src/test/Problem/P035Spec.hs
@@ -18,6 +18,18 @@ spec = do
     it "returns [123,231,312] when n = 123" $ do
       cycles 123 `shouldBe` [123, 231, 312]
 
+  describe "allDigitsOdd" $ do
+    it "returns True when n = 11" $ do
+      allDigitsOdd 11 `shouldBe` True
+    it "returns False when n = 12" $ do
+      allDigitsOdd 12 `shouldBe` False
+    it "returns False when n = 51" $ do
+      allDigitsOdd 51 `shouldBe` False
+    it "returns True when n = 1379" $ do
+      allDigitsOdd 1379 `shouldBe` True
+    it "returns False when n = 13579" $ do
+      allDigitsOdd 13579 `shouldBe` False
+
   describe "solve" $ do
     it "returns 13 when n = 100" $ do
       solve 100 `shouldBe` 13

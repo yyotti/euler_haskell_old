f8db587be1bade96b18d553d81dbd1544b39470d
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-25 20:19:23 +0900

    [P037] 回答

--- src/main/Problem/P037.hs
+++ src/main/Problem/P037.hs
@@ -1,4 +1,8 @@
 module Problem.P037 where
+import Common.Util
+import Control.Monad
+import Data.List
+import Common.Arithmetic
 
 {-
 - Truncatable primes
@@ -7,8 +11,32 @@ module Problem.P037 where
 
 {-
 - [方針1]
+- 問題によると11個しかないということなので、素数を順に調べていって
+- 切り詰め素数が11個になったところでやめる。
 -
 - [結果]
+- 748317
+- time:0.811496s
 -}
+
+trimr :: Integral a => a -> a
+trimr = flip div 10
+
+triml :: Integral a => a -> a
+-- triml n = n `mod` 10^e
+--   where e = (length $ digits n) - 1
+-- ↑をポイントフリーで書くと↓になるが、やりすぎか？
+triml = ap (flip mod . (10^) . e) id
+  where e = flip (-) 1 . length . digits
+
+truncates :: Integral a => a -> [a]
+truncates = nub . ap ((++) . rs) ls
+  where rs = takeWhile (> 0) . iterate trimr
+        ls = takeWhile (> 0) . iterate triml
+
+isTruncatablePrime :: Integral a => a -> Bool
+isTruncatablePrime = ap ((&&) . (>= 10)) (all isPrime . truncates)
+
 solve :: Int -> Integer
-solve _ = undefined
+solve = toInteger . sum . flip take truncatablePrimes . min 11
+  where truncatablePrimes = filter isTruncatablePrime primes
--- src/test/Problem/P037Spec.hs
+++ src/test/Problem/P037Spec.hs
@@ -8,6 +8,54 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "trimr" $ do
+    it "returns 0 when n = 1" $ do
+      trimr 1 `shouldBe` 0
+    it "returns 0 when n = 9" $ do
+      trimr 9 `shouldBe` 0
+    it "returns 3 when n = 39" $ do
+      trimr 39 `shouldBe` 3
+    it "returns 53 when n = 539" $ do
+      trimr 539 `shouldBe` 53
+
+  describe "triml" $ do
+    it "returns 0 when n = 1" $ do
+      triml 1 `shouldBe` 0
+    it "returns 0 when n = 9" $ do
+      triml 9 `shouldBe` 0
+    it "returns 9 when n = 39" $ do
+      triml 39 `shouldBe` 9
+    it "returns 39 when n = 539" $ do
+      triml 539 `shouldBe` 39
+
+  describe "truncates" $ do
+    it "returns [1] when n = 1" $ do
+      truncates 1 `shouldBe` [1]
+    it "returns [9] when n = 9" $ do
+      truncates 9 `shouldBe` [9]
+    it "returns [39,3,9] when n = 39" $ do
+      truncates 39 `shouldBe` [39,3,9]
+    it "returns [539,53,5,39,9] when n = 539" $ do
+      truncates 539 `shouldBe` [539, 53, 5, 39, 9]
+    it "returns [1111,111,11,1] when n = 1111" $ do
+      truncates 1111 `shouldBe` [1111, 111, 11, 1]
+
+  describe "isTruncatablePrime" $ do
+    it "returns False when n = 2" $ do
+      isTruncatablePrime 2 `shouldBe` False
+    it "returns False when n = 3" $ do
+      isTruncatablePrime 3 `shouldBe` False
+    it "returns False when n = 5" $ do
+      isTruncatablePrime 5 `shouldBe` False
+    it "returns False when n = 7" $ do
+      isTruncatablePrime 7 `shouldBe` False
+    it "returns False when n = 17" $ do
+      isTruncatablePrime 17 `shouldBe` False
+    it "returns True when n = 37" $ do
+      isTruncatablePrime 37 `shouldBe` True
+    it "returns True when n = 3797" $ do
+      isTruncatablePrime 3797 `shouldBe` True
+
   describe "solve" $ do
     it "returns 23 when n = 1" $ do
       solve 1 `shouldBe` 23

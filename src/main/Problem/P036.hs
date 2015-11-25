021ac99d9f10262a4d1c17e8034bbe442638622d
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-25 18:05:01 +0900

    [P036] ちょっとだけ高速化

--- src/main/Problem/P036.hs
+++ src/main/Problem/P036.hs
@@ -15,6 +15,17 @@ import Common.Util
 - time:0.099149s
 -}
 
+{-
+- [方針2]
+- 2進数表記したときに回文数になるためには奇数でなければならない。
+- 0は偶数で条件を満たすが、和をとった場合には無くとも問題ないので、
+- 1から始まる奇数についてのみ判定していけばよい。
+-
+- [結果]
+- 872187
+- time:0.062355s
+-}
+
 bin :: Integral a => a -> String
 bin 0 = "0"
 bin 1 = "1"
@@ -22,4 +33,4 @@ bin n = bin m ++ bin r
   where (m, r) = n `divMod` 2
 
 solve :: Int -> Integer
-solve n = toInteger $ sum $ filter (\ i -> isPalindrome (show i) && isPalindrome (bin i)) [0..(n-1)]
+solve n = toInteger $ sum $ filter (\ i -> isPalindrome (show i) && isPalindrome (bin i)) [1,3..(n-1)]

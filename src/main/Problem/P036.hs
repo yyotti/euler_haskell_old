675952e7bbd1430b47193380c3f56094e4c3d4ee
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-25 17:38:01 +0900

    [P036] 回答

--- src/main/Problem/P036.hs
+++ src/main/Problem/P036.hs
@@ -1,4 +1,5 @@
 module Problem.P036 where
+import Common.Util
 
 {-
 - Double-base palindromes
@@ -7,8 +8,18 @@ module Problem.P036 where
 
 {-
 - [方針1]
+- 0から順に2進数表記に変換し、回文数であるか判定する。
 -
 - [結果]
+- 872187
+- time:0.099149s
 -}
+
+bin :: Integral a => a -> String
+bin 0 = "0"
+bin 1 = "1"
+bin n = bin m ++ bin r
+  where (m, r) = n `divMod` 2
+
 solve :: Int -> Integer
-solve _ = undefined
+solve n = toInteger $ sum $ filter (\ i -> isPalindrome (show i) && isPalindrome (bin i)) [0..(n-1)]
--- src/test/Problem/P036Spec.hs
+++ src/test/Problem/P036Spec.hs
@@ -8,8 +8,22 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "bin" $ do
+    it "returns \"0\" when n = 0" $ do
+      bin 0 `shouldBe` "0"
+    it "returns \"1\" when n = 1" $ do
+      bin 1 `shouldBe` "1"
+    it "returns \"10\" when n = 2" $ do
+      bin 2 `shouldBe` "10"
+    it "returns \"10\" when n = 3" $ do
+      bin 3 `shouldBe` "11"
+    it "returns \"100\" when n = 4" $ do
+      bin 4 `shouldBe` "100"
+    it "returns \"101\" when n = 5" $ do
+      bin 5 `shouldBe` "101"
+
   describe "solve" $ do
     it "returns 25 when n = 10" $ do
       solve 10 `shouldBe` 25
-    it "returns 743 when n = 100" $ do
-      solve 25 `shouldBe` 743
+    it "returns 157 when n = 100" $ do
+      solve 100 `shouldBe` 157

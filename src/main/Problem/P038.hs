f86b4a4477b9f0090ac96e09ab96c9b6964a535d
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-26 15:57:09 +0900

    [P038] 回答

--- src/main/Problem/P038.hs
+++ src/main/Problem/P038.hs
@@ -1,4 +1,6 @@
 module Problem.P038 where
+import Common.Util
+import Control.Monad
 
 {-
 - Pandigital multiples
@@ -7,8 +9,33 @@ module Problem.P038 where
 
 {-
 - [方針1]
+- 掛ける数は (1,2,...,n) (n > 1) なので、2以上である。
+- d桁の整数に1桁の数字を掛け合わせると、d桁もしくは(d + 1)桁となる。
+- 掛けられる数をi、その桁数をdとすると、
+-   d = 5の場合
+-     i x 1 -> 5桁
+-     i x 2 -> 5桁 or 6桁
+- となるため、連結積が9桁のパンデジタル数になることはない。
+-
+-   d = 4の場合
+-     i x 1 -> 4桁
+-     i x 2 -> 4桁 or 5桁
+- であるから、掛けられる数が4桁以下の整数で考えればよい。
 -
 - [結果]
+- 932718654
+- time:0.020963s
 -}
+
+concatNum :: Integral a => a -> a -> a
+-- concatNum a b = a * 10^(e b) + b
+-- ↓やりすぎな気はする
+concatNum = ap (+) . flip (.) ((10^) . e) . (*)
+  where e = length . digits
+
+concatProducts :: Integral a => a -> [a]
+concatProducts = ap (scanl concatNum) (flip map [2..] . (*))
+
 solve :: Integer
-solve = undefined
+solve = (toInteger . maximum . filter (isPandigital . (:[])) . map cp) [1..9999]
+  where cp = head . dropWhile (< 10^8) . concatProducts
--- src/test/Problem/P038Spec.hs
+++ src/test/Problem/P038Spec.hs
@@ -8,6 +8,24 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "concatNum" $ do
+    it "returns 10 when (a,b) = (1,0)" $ do
+      concatNum 1 0 `shouldBe` 10
+    it "returns 1 when (a,b) = (0,1)" $ do
+      concatNum 0 1 `shouldBe` 1
+    it "returns 12 when (a,b) = (1,2)" $ do
+      concatNum 1 2 `shouldBe` 12
+    it "returns 123 when (a,b) = (12,3)" $ do
+      concatNum 12 3 `shouldBe` 123
+    it "returns 321 when (a,b) = (3,21)" $ do
+      concatNum 3 21 `shouldBe` 321
+
+  describe "concatProducts" $ do
+    it "first 10 terms are [1,12,123,...,123456789,12345678910] when n = 1" $ do
+      take 10 (concatProducts 1) `shouldBe` [1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 12345678910]
+    it "first 5 terms are [2,24,246,2468,246810] when n = 2" $ do
+      take 5 (concatProducts 2) `shouldBe` [2, 24, 246, 2468, 246810]
+
   describe "solve" $ do
     it "is 932718654" $ do
       solve `shouldBe` 932718654

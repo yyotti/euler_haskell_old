b1000d35582742ddf4f12f467c0abccaf47eb923
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-26 20:25:43 +0900

    [P039] 回答

--- src/main/Problem/P039.hs
+++ src/main/Problem/P039.hs
@@ -1,4 +1,6 @@
 module Problem.P039 where
+import Data.Ord
+import Data.List
 
 {-
 - Integer right triangles
@@ -7,8 +9,35 @@ module Problem.P039 where
 
 {-
 - [方針1]
+- (a, b, c)はピタゴラス数になるので、
+-   a^2 + b^2 = c^2
+- を満たす。また、a = bの場合は
+-   2a^2 = c^2
+- となり、cが整数にならないため、a /= b である。
+-
+- a < b < c とする。
+-   a + b + c = p
+- であることから、下記の関係が成立する。
+-   a <= p / 3 (小数点以下は切り捨て)
+-   p / 3 <= b <= 2p / 3  (小数点以下は切り捨て)
+-   c = p - a - b
+- これを元にして (a, b, c) の組を作り、ピタゴラス数となっている
+- 組の個数が最大となるpを探せばよい。
 -
 - [結果]
+- 840
+- time:1.360704s
 -}
+
+isPythagoreanTriples :: Integral a => a -> a -> a -> Bool
+isPythagoreanTriples a b c = a^2 + b^2 == c^2
+
+pythatoreanTripletCount :: Integral a => a -> Int
+pythatoreanTripletCount p = length $ filter (\ (a, b, c) -> isPythagoreanTriples a b c) $ triplet p
+  where triplet p = concatMap (\ a -> map (\ b -> (a, b, p - a - b)) [aMax p .. bMax p]) [1 .. aMax p]
+        aMax = flip div 3
+        bMax = flip div 3 . (*) 2
+
 solve :: Int -> Integer
-solve _ = undefined
+solve n = toInteger $ fst $ maximumBy (comparing snd) $ map (\ p -> (p, pythatoreanTripletCount p)) [1 .. n]
+
--- src/test/Problem/P039Spec.hs
+++ src/test/Problem/P039Spec.hs
@@ -8,6 +8,32 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "isPythagoreanTriples" $ do
+    it "returns False when (a,b,c) = (1,2,3)" $ do
+      isPythagoreanTriples 1 2 3 `shouldBe` False
+    it "returns True when (a,b,c) = (3,4,5)" $ do
+      isPythagoreanTriples 3 4 5 `shouldBe` True
+    it "returns True when (a,b,c) = (5,12,13)" $ do
+      isPythagoreanTriples 5 12 13 `shouldBe` True
+    it "returns True when (a,b,c) = (6,8,10)" $ do
+      isPythagoreanTriples 6 8 10 `shouldBe` True
+
+  describe "pythatoreanTripletCount" $ do
+    it "returns 0 when p = 1" $ do
+      pythatoreanTripletCount 1 `shouldBe` 0
+    it "returns 0 when p = 11" $ do
+      pythatoreanTripletCount 11 `shouldBe` 0
+    it "returns 1 when p = 12" $ do
+      pythatoreanTripletCount 12 `shouldBe` 1
+    it "returns 1 when p = 24" $ do
+      pythatoreanTripletCount 24 `shouldBe` 1
+    it "returns 3 when p = 120" $ do
+      pythatoreanTripletCount 120 `shouldBe` 3
+
   describe "solve" $ do
     it "returns 12 when n = 12" $ do
       solve 12 `shouldBe` 12
+    it "returns 24 when n = 24" $ do
+      solve 24 `shouldBe` 24
+    it "returns 120 when n = 130" $ do
+      solve 130 `shouldBe` 120

68e9d23f8b6a7f11d53ce1c70241c697ac853c66
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-27 19:59:37 +0900

    [P043] 回答

--- src/main/Problem/P043.hs
+++ src/main/Problem/P043.hs
@@ -1,4 +1,8 @@
 module Problem.P043 where
+import Common.Arithmetic
+import Common.Util
+import Data.List
+import Control.Monad
 
 {-
 - Sub-string divisibility
@@ -7,8 +11,24 @@ module Problem.P043 where
 
 {-
 - [方針1]
+- 0から9のパンデジタル数を1つ1つ調べていく。
 -
 - [結果]
+- 16695334890
+- time:2.219413s
 -}
+
+sliding :: Int -> [a] -> [[a]]
+sliding _ [] = []
+sliding n ls@(_:xs) | length ls <= n = [ls]
+                    | otherwise = take n ls : sliding n xs
+
+slidingNums :: Integral a => Int -> [a] -> [a]
+slidingNums = (map toNum .) . sliding
+
+isDivisiblePandigital :: Integral a => [a] -> Bool
+isDivisiblePandigital = all (== 0) . zipWith (flip mod) primes . (tail . slidingNums 3)
+
 solve :: Integer
-solve = undefined
+solve = (toInteger . sum . map toNum) divisiblePandigitals
+  where divisiblePandigitals = (filter (ap ((&&) . ((/= 0) . head)) isDivisiblePandigital) . permutations) [0..9]
--- src/test/Problem/P043Spec.hs
+++ src/test/Problem/P043Spec.hs
@@ -8,6 +8,38 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "sliding" $ do
+    it "returns [] when (n,ls) = (2,[])" $ do
+      sliding 2 "" `shouldBe` []
+    it "returns [[1]] when (n,ls) = (2,[1])" $ do
+      sliding 2 [1] `shouldBe` [[1]]
+    it "returns [[1],[2]] when (n,ls) = (1,[1,2])" $ do
+      sliding 1 [1, 2] `shouldBe` [[1], [2]]
+    it "returns [[1,2]] when (n,ls) = (2,[1,2])" $ do
+      sliding 2 [1, 2] `shouldBe` [[1, 2]]
+    it "returns [[1,2],[2,3]] when (n,ls) = (2,[1,2,3])" $ do
+      sliding 2 [1, 2, 3] `shouldBe` [[1, 2], [2, 3]]
+    it "returns [[1,2,3],[2,3,4],[3,4,5]] when (n,ls) = (3,[1,2,3,4,5])" $ do
+      sliding 3 [1, 2, 3, 4, 5] `shouldBe` [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+
+  describe "slidingNums" $ do
+    it "returns [1] when (n,ls) = (2,[1])" $ do
+      slidingNums 2 [1] `shouldBe` [1]
+    it "returns [1,2] when (n,ls) = (1,[1,2])" $ do
+      slidingNums 1 [1, 2] `shouldBe` [1, 2]
+    it "returns [12] when (n,ls) = (2,[1,2])" $ do
+      slidingNums 2 [1, 2] `shouldBe` [12]
+    it "returns [12,23] when (n,ls) = (2,[1,2,3])" $ do
+      slidingNums 2 [1, 2, 3] `shouldBe` [12, 23]
+    it "returns [123,234,345] when (n,ls) = (3,[1,2,3,4,5])" $ do
+      slidingNums 3 [1, 2, 3, 4, 5] `shouldBe` [123, 234, 345]
+
+  describe "isDivisiblePandigital" $ do
+    it "returns True when ls = [1,4,0,6,3,5,7,2,8,9]" $ do
+      isDivisiblePandigital [1, 4, 0, 6, 3, 5, 7, 2, 8, 9] `shouldBe` True
+    it "returns False when ls = [1,4,0,6,3,5,7,2,8,9]" $ do
+      isDivisiblePandigital [1, 4, 0, 6, 3, 5, 7, 2, 9, 8] `shouldBe` False
+
   describe "solve" $ do
     it "is 16695334890" $ do
       solve `shouldBe` 16695334890

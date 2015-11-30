9dcd6f19923eb32bf49a27c9fa6bc3335f5ad051
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-11-30 12:04:01 +0900

    [P044] 高速化

--- src/main/Problem/P044.hs
+++ src/main/Problem/P044.hs
@@ -1,7 +1,7 @@
 module Problem.P044 where
-import Control.Monad
 import Data.List
 import Data.Maybe
+import Common.Arithmetic
 
 {-
 - Pentagon numbers
@@ -15,13 +15,33 @@ import Data.Maybe
 - [結果]
 - 5482660
 - time:42.819109s
+-
+- [コミット]
+- 5c842fc
+-}
+
+{-
+- [方針2]
+- 整数mが五角数か否かを調べる関数が遅すぎるので工夫する。
+-
+-   P(n) = n(3n-1)/2 = m
+- として、
+-   3n^2 - n - 2m = 0
+- が整数解を持てばよい。それには判別式をdとして
+-   d = 1 - 4*3*(-2m) = 1 + 24m
+-   n = (1 + √d)/6
+- が整数になれば五角数と判定できる。
+-
+- [結果]
+- 5482660
+- time:0.353837s
 -}
 
 pentagonals :: Integral a => [a]
 pentagonals = 1 : zipWith (+) pentagonals [4,7..]
 
 isPentagonNumber :: Integral a => a -> Bool
-isPentagonNumber = ap (==) (head . flip dropWhile pentagonals . (>))
+isPentagonNumber = isInteger . (/6) . (1+) . sqrt . fromIntegral . (1+) . (24*)
 
 solve :: Integer
 solve = (toInteger . minimum . fromJust . find ((> 0) . length) . map pentagonalPairs) [1..]

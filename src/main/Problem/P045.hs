2f011429dfc83a18a89c759be227a16772e2645d
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-12-04 15:20:24 +0900

    [P045] 回答

--- src/main/Problem/P045.hs
+++ src/main/Problem/P045.hs
@@ -7,8 +7,27 @@ module Problem.P045 where
 
 {-
 - [方針1]
+- 三角数、五角数、六角数の列をそれぞれ生成し、指定の数以上になるよう
+- 調整した後で、等しくなるものを探していく
 -
 - [結果]
+- 1533776805
+- time:0.021379s
 -}
+
+triangles :: Integral a => [a]
+triangles = 1 : zipWith (+) triangles [2..]
+
+pentagonals :: Integral a => [a]
+pentagonals = 1 : zipWith (+) pentagonals [4,7..]
+
+hexagonals :: Integral a => [a]
+hexagonals = 1 : zipWith (+) hexagonals [5,9..]
+
+findNextSame :: Integral a => [a] -> [a] -> [a] -> a
+findNextSame (t:ts) ps hs | head (dropWhile (< t) ps) == t && head (dropWhile (< t) hs) == t = t
+                          | otherwise = findNextSame ts (dropWhile (< t) ps) (dropWhile (< t) hs)
+findNextSame _ _ _ = 0 -- ここには入らないはず
+
 solve :: Int -> Integer
-solve _ = undefined
+solve n = toInteger $ findNextSame (dropWhile (<= n) triangles) (dropWhile (<= n) pentagonals) (dropWhile (<= n) hexagonals)
--- src/test/Problem/P045Spec.hs
+++ src/test/Problem/P045Spec.hs
@@ -8,6 +8,24 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "triangles" $ do
+    it "first 10 terms are [1,3,6,10,15,21,28,36,45,55]" $ do
+      take 10 triangles `shouldBe` [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+
+  describe "pentagonals" $ do
+    it "first 10 terms are [1,5,12,22,35,51,70,92,117,145]" $ do
+      take 10 pentagonals `shouldBe` [1, 5, 12, 22, 35, 51, 70, 92, 117, 145]
+
+  describe "triangles" $ do
+    it "first 10 terms are [1,6,15,28,45,66,91,120,153,190]" $ do
+      take 10 hexagonals `shouldBe` [1, 6, 15, 28, 45, 66, 91, 120, 153, 190]
+
+  describe "findNextSame" $ do
+    it "returns 3 when (ts,ps,hs) = ([1,3,5],[1,2,3],[0,3])" $ do
+      findNextSame [1, 3, 5] [1, 2, 3] [0, 3] `shouldBe` 3
+
   describe "solve" $ do
-    it "returns 40755 when n = 0" $ do
-      solve 0 `shouldBe` 40755
+    it "returns 1 when n = 0" $ do
+      solve 0 `shouldBe` 1
+    it "returns 40755 when n = 1" $ do
+      solve 1 `shouldBe` 40755
